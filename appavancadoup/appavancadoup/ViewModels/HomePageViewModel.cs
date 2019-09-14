04b8af96e5de24f5e38f15c76e1e25a652988fb7
Diego Wojcik <diegorafael85@gmail.com>
2019-09-14 10:42:06 -0300

    :+1:  COm view modelbase

--- appavancadoup/appavancadoup/ViewModels/HomePageViewModel.cs
+++ appavancadoup/appavancadoup/ViewModels/HomePageViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace appavancadoup.ViewModels
 {
-    public sealed class HomePageViewModel : BindableBase
+    public sealed class HomePageViewModel : ViewModelBase
     {
         private string title;
+
+        public HomePageViewModel(INavigationService navigationService
+            , IPageDialogService pageDialogService)
+            : base(navigationService, pageDialogService)
+        {
+            Title = "Collect App";
+        }
+
         public string Title
         {
             get => title;
