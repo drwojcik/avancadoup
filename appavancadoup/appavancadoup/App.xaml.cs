f8d949a530bed826ec3a88869c813467e6fcf6c9
Diego Wojcik <diegorafael85@gmail.com>
2019-09-14 10:18:51 -0300

    Prism habilitado
    Prism Habilitado e imprimindo página em branco

--- appavancadoup/appavancadoup/App.xaml.cs
+++ appavancadoup/appavancadoup/App.xaml.cs
@@ -1,31 +1,39 @@
-﻿using System;
+﻿using appavancadoup.Views;
+using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace appavancadoup
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        //PAra poder usar componentes do xamarin forms junto e não somente o Prism
+        public App() : this(null) { }
+        public App(IPlatformInitializer platformInitializer)
+            : base(platformInitializer, true)
+        {
+        }
+
+        public App(IPlatformInitializer platformInitializer, bool setFormsDependencyResolver)
+            : base(platformInitializer, setFormsDependencyResolver)
+        {
+        }
+
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<HomePage>();
+        }
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
+            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(HomePage)}");
 
-            MainPage = new MainPage();
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
         }
     }
 }
---
+++ appavancadoup/appavancadoup/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appavancadoup.ViewModels
+{
+    public sealed class HomePageViewModel : BindableBase
+    {
+        private string title;
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+    }
+}
